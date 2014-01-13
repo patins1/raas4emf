a155b7ae52f9828b6e185bbec6a1ca75ffc28361
patins <patins@f81cfaeb-dc96-476f-bd6d-b0d16e38694e>
2014-01-13 13:01:53 +0000

    added entry point project for the embedded 3d viewer

---
+++ trunk/org.raas4emf.embedded.ui/src/org/raas4emf/embedded/ui/Startup.java
@@ -0,0 +1,13 @@
+package org.raas4emf.embedded.ui;
+
+import org.eclipse.ui.IStartup;
+
+public class Startup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
