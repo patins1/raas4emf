bb8ab7c4901ad1beefb8a3d194c849cf2f55867f
patins <patins@f81cfaeb-dc96-476f-bd6d-b0d16e38694e>
2014-02-08 14:54:57 +0000

    config DB server by parameter

--- plugins/org.raas4emf.cms.ui/src/org/raas4emf/cms/ui/CMSActivator.java
+++ plugins/org.raas4emf.cms.ui/src/org/raas4emf/cms/ui/CMSActivator.java
@@ -3,12 +3,20 @@
  */
 package org.raas4emf.cms.ui;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.StringBufferInputStream;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.emf.cdo.internal.server.bundle.CDOServerApplication;
 import org.eclipse.net4j.internal.util.bundle.AbstractPlatform;
 import org.eclipse.osgi.framework.internal.core.FrameworkProperties;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.raas4emf.cms.core.FileUtil;
 import org.raas4emf.cms.core.RAASUtils;
 import org.raas4emf.cms.ui.discriminator.IRAASSessionSingletonService;
 import org.raas4emf.cms.ui.discriminator.RAASSessionSingleton;
@@ -35,8 +43,45 @@ public class CMSActivator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		CMSActivator.context = bundleContext;
 
-		if (FrameworkProperties.getProperty(AbstractPlatform.SYSTEM_PROPERTY_NET4J_CONFIG) == null)
-			FrameworkProperties.setProperty(AbstractPlatform.SYSTEM_PROPERTY_NET4J_CONFIG, RAASUtils.ROOTPATH + "/raasserver/config");
+		URL fileURL = new URL("platform:/plugin/org.raas4emf.server/config/cdo-server.template.xml");
+		URL u = FileLocator.resolve(fileURL);
+		String fileName = u.getFile();
+		File file = new File(fileName);
+		String contents = FileUtil.inputstreamToString(new FileInputStream(file));
+
+		String jdbc = System.getProperty("JDBC_CONNECTION_STRING");
+		if (jdbc == null) {
+			String dbName = System.getProperty("RDS_DB_NAME");
+			String userName = System.getProperty("RDS_USERNAME");
+			String password = System.getProperty("RDS_PASSWORD");
+			String hostname = System.getProperty("RDS_HOSTNAME");
+			String port = System.getProperty("RDS_PORT");
+			if (hostname != null)
+				jdbc = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+		}
+		if (jdbc == null)
+			jdbc = "jdbc:hsqldb:mem:repo1";
+		System.out.println("JDBC_CONNECTION_STRING=" + jdbc);
+
+		for (String db : new String[] { "h2", "hsqldb", "postgresql" }) {
+			if (jdbc.contains(db)) {
+				int fromIndex = contents.indexOf("<!--<dbAdapter name=\"" + db);
+				contents = contents.substring(0, fromIndex) + contents.substring(fromIndex + "<!--".length());
+				contents = replaceAttribute(contents, fromIndex, "url", jdbc);
+				contents = replaceAttribute(contents, fromIndex, "URL", jdbc);
+				FileUtil.inputstreamToOutputstream(new StringBufferInputStream(contents), new FileOutputStream(new File(file.getParentFile(), "cdo-server.xml")));
+				System.out.println("Created cdo-server.xml from template using " + jdbc);
+			}
+		}
+		System.out.println("Read config file from " + file.getParentFile().toString());
+		FrameworkProperties.setProperty(AbstractPlatform.SYSTEM_PROPERTY_NET4J_CONFIG, file.getParentFile().toString());
+
+		try {
+			RAASUtils.getRAASProp("RAASSERVER");
+		} catch (Exception e) {
+			// not defined, set default
+			System.setProperty("RAASSERVER", "localhost:2036");
+		}
 
 		Thread t = new Thread() {
 			@Override
@@ -51,6 +96,16 @@ public class CMSActivator implements BundleActivator {
 
 		};
 		t.start();
+	}
+
+	private String replaceAttribute(String contents, int fromIndex, String attribute, String value) {
+		int index = contents.indexOf(attribute + "=\"", fromIndex);
+		if (index >= 0 && index < contents.indexOf("-->", fromIndex)) {
+			index += (attribute + "=\"").length();
+			value = value.replace("&", "&amp;");
+			return contents.substring(0, index) + value + contents.substring(contents.indexOf('"', index));
+		}
+		return contents;
 	}
 
 	/*
