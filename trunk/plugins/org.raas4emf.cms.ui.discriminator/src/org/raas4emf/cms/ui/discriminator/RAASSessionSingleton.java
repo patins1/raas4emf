19b2908da684b54a5e38311875588f0ee61ad228
patins <patins@f81cfaeb-dc96-476f-bd6d-b0d16e38694e>
2014-03-13 11:26:59 +0000

    refactor dependencies

--- trunk/plugins/org.raas4emf.cms.ui.discriminator/src/org/raas4emf/cms/ui/discriminator/RAASSessionSingleton.java
+++ trunk/plugins/org.raas4emf.cms.ui.discriminator/src/org/raas4emf/cms/ui/discriminator/RAASSessionSingleton.java
@@ -13,7 +13,6 @@ import org.eclipse.emf.cdo.net4j.CDONet4jSession;
 import org.eclipse.emf.cdo.view.CDOView;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.jface.viewers.ISelection;
 import org.raas4emf.cms.core.RAASUtils;
 
 import raascms.Artifact;
@@ -260,7 +259,7 @@ abstract public class RAASSessionSingleton {
 	public void executeJSMethod(String methodName, String stringArg) {
 	}
 
-	public boolean propagateTreeSelection(ISelection selection, boolean isDblClick) {
+	public boolean propagateTreeSelection(Object selection, boolean isDblClick) {
 		return false;
 	}
 
