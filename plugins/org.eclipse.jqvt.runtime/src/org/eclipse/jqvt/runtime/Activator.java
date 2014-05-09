d2709de38a33f6db4f5ceb952ad44d64ed57d479
patins <patins@f81cfaeb-dc96-476f-bd6d-b0d16e38694e>
2014-05-09 07:32:54 +0000

    make logger work in non-osgi-mode

--- plugins/org.eclipse.jqvt.runtime/src/org/eclipse/jqvt/runtime/Activator.java
+++ plugins/org.eclipse.jqvt.runtime/src/org/eclipse/jqvt/runtime/Activator.java
@@ -1,5 +1,6 @@
 package org.eclipse.jqvt.runtime;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
@@ -48,21 +49,39 @@ public class Activator extends Plugin {
 	}
 
 	public static void log(String msg) {
-		getDefault().getLog().log(new Status(Status.OK, getDefault().getBundle().getSymbolicName(), Status.OK, msg, null));
+		log(new Status(Status.OK, getSymbolicName(), Status.OK, msg, null));
 	}
 
 	public static void err(String msg) {
-		getDefault().getLog().log(new Status(Status.ERROR, getDefault().getBundle().getSymbolicName(), Status.OK, msg, null));
+		log(new Status(Status.ERROR, getSymbolicName(), Status.OK, msg, null));
 
 	}
 
 	public static void err(String msg, Throwable e) {
-		getDefault().getLog().log(new Status(Status.ERROR, getDefault().getBundle().getSymbolicName(), Status.OK, msg, e));
+		log(new Status(Status.ERROR, getSymbolicName(), Status.OK, msg, e));
 
 	}
 
 	public static void err(Exception e) {
-		getDefault().getLog().log(new Status(Status.ERROR, getDefault().getBundle().getSymbolicName(), Status.OK, e.getMessage(), e));
+		log(new Status(Status.ERROR, getSymbolicName(), Status.OK, e.getMessage(), e));
+	}
+
+	public static void log(IStatus status) {
+		if (getDefault() != null && getDefault().getLog() != null) {
+			getDefault().getLog().log(status);
+			return;
+		}
+		if (status.isOK()) {
+			System.out.println(status.getMessage());
+			return;
+		}
+		System.err.println(status.getMessage());
+		if (status.getException() != null)
+			status.getException().printStackTrace();
+	}
+
+	private static String getSymbolicName() {
+		return "org.eclipse.jqvt.runtime";
 	}
 
 }
