3a5c87ec01e9936dd7dd7eb0e2f9e349624163c9
patins <patins@f81cfaeb-dc96-476f-bd6d-b0d16e38694e>
2014-05-09 07:25:47 +0000

    refactored junit dependencies

--- plugins/org.eclipse.jqvt.examples.shapes/bin/org/eclipse/jqvt/examples/shapes/r11/mydata.js
+++
@@ -1,6 +0,0 @@
-{
-	"IfcAdjacent.001" : [ "Cube.002", "Cube" ],
-	"ContentDescription" : "Blender Adjancency Data",
-	"IfcAdjacent" : [ "Cube.002", "Cube.001" ],
-	"IfcAdjacent.002" : [ "Cube.003", "Suzanne.001" ]
-}
