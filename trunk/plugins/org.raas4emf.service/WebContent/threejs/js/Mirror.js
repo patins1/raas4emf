936594dcf40731aeaa8bcdcd9534f10e2f109194
patins <patins@f81cfaeb-dc96-476f-bd6d-b0d16e38694e>
2015-01-25 10:20:32 +0000

    updated to blender v70

--- trunk/plugins/org.raas4emf.service/WebContent/threejs/js/Mirror.js
+++ trunk/plugins/org.raas4emf.service/WebContent/threejs/js/Mirror.js
@@ -110,6 +110,7 @@ THREE.Mirror = function ( renderer, camera, options ) {
 	this.textureMatrix = new THREE.Matrix4();
 
 	this.mirrorCamera = this.camera.clone();
+	this.mirrorCamera.matrixAutoUpdate = true;
 
 	this.texture = new THREE.WebGLRenderTarget( width, height );
 	this.tempTexture = new THREE.WebGLRenderTarget( width, height );
@@ -142,6 +143,7 @@ THREE.Mirror = function ( renderer, camera, options ) {
 };
 
 THREE.Mirror.prototype = Object.create( THREE.Object3D.prototype );
+THREE.Mirror.prototype.constructor = THREE.Mirror;
 
 THREE.Mirror.prototype.renderWithMirror = function ( otherMirror ) {
 
