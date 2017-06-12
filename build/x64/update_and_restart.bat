f028815ab6560a0e4e7d35c46168675729c0e126
Joerg Kiegeland <joerg@kiegeland.com>
2017-06-12 11:20:04 +1000

    localize absolute path

--- build/x64/update_and_restart.bat
+++ build/x64/update_and_restart.bat
@@ -1,20 +1,20 @@
 REM ################################### Kill server processes
-start /WAIT /D C:\bim\NOLServer\apache-tomcat\bin\ shutdown.bat
+start /WAIT /D .\apache-tomcat\bin\ shutdown.bat
 REM ################################### Update NOLServer from subversion repository
 svn cleanup c:\bim\NOLServer
 REM svn update c:\bim\NOLServer
 REM ################################### Remove web applications
-rmdir /S /Q C:\bim\NOLServer\apache-tomcat\webapps\org.raas4emf.service
-rmdir /S /Q C:\bim\NOLServer\apache-tomcat\webapps\qut.bim.raas
-rmdir /S /Q C:\bim\NOLServer\apache-tomcat\webapps\qut.bim.fileserver
-rmdir /S /Q C:\bim\NOLServer\apache-tomcat\webapps\raascms
-rmdir /S /Q C:\bim\NOLServer\apache-tomcat\work\Catalina\localhost\org.raas4emf.service
-rmdir /S /Q C:\bim\NOLServer\apache-tomcat\work\Catalina\localhost\qut.bim.raas
-rmdir /S /Q C:\bim\NOLServer\apache-tomcat\work\Catalina\localhost\qut.bim.fileserver
-rmdir /S /Q C:\bim\NOLServer\apache-tomcat\work\Catalina\localhost\raascms
+rmdir /S /Q .\apache-tomcat\webapps\org.raas4emf.service
+rmdir /S /Q .\apache-tomcat\webapps\qut.bim.raas
+rmdir /S /Q .\apache-tomcat\webapps\qut.bim.fileserver
+rmdir /S /Q .\apache-tomcat\webapps\raascms
+rmdir /S /Q .\apache-tomcat\work\Catalina\localhost\org.raas4emf.service
+rmdir /S /Q .\apache-tomcat\work\Catalina\localhost\qut.bim.raas
+rmdir /S /Q .\apache-tomcat\work\Catalina\localhost\qut.bim.fileserver
+rmdir /S /Q .\apache-tomcat\work\Catalina\localhost\raascms
 REM ################################### Start database server
-REM start /D C:\bim\NOLServer\bimserver\ bimserver.exe -debug
+REM start /D .\bimserver\ bimserver.exe -debug
 REM ################################### Start web server
-start /D C:\bim\NOLServer\apache-tomcat\bin\ startup.bat
+start /D .\apache-tomcat\bin\ startup.bat
 REM ################################### Finished! But note, don't use the NOL server before the database server prints "Application Started" and the web server prints "Server startup" in their console windows!
 timeout /T 30
